d750ca5792f28f4f0be9c2fd938e8f4c0451c240
heidibeth <93995431+heidibeth@users.noreply.github.com>
2022-02-08 09:44:47 -0500

    added baseURL and key

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 
+const baseURL = `https://api.openweathermap.org/data/2.5/weather`;
+
 const Weather = (props) => {
+    const key = 'd36a46dc51df18c7b4c1110ab98d8e5f';
+    let url = `${baseURL}?lat=39.7914&lon=-86.1480&appid=${key}`
+
 
 async function handleFetch() {
 
     try{
-
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        
     } catch (err) {
         console.error(err);
     }}
@@ -16,7 +24,10 @@ useEffect(() => {
 
     return ( 
     <div>
-        <h1>Welcome to Weather</h1>
+        <h1>Indianapolis</h1>
+        <p>Temperature: ºF</p>
+        <p>Temperature: ºC</p>
+            <button>Change Unit</button>
     </div> 
     );
     }
