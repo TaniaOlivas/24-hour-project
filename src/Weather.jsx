8b61b91019e6be0045232815a3f71edf5139ea3d
heidibeth <93995431+heidibeth@users.noreply.github.com>
2022-02-08 09:56:34 -0500

    Units of temp added. Temp data inserted

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const baseURL = `https://api.openweathermap.org/data/2.5/weather`;
 
 const Weather = (props) => {
     const key = 'd36a46dc51df18c7b4c1110ab98d8e5f';
-    let url = `${baseURL}?lat=39.7914&lon=-86.1480&appid=${key}`
+    let url = `${baseURL}?lat=39.7914&lon=-86.1480&appid=${key}`;
+    const [temp, setTemp] = useState('Loading...');
+    let celsius = Math.floor (temp - 273);
+    let fahrenheit = Math.floor (celsius * (9/5) + 32);
 
 
 async function handleFetch() {
@@ -12,8 +15,9 @@ async function handleFetch() {
     try{
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        
+        console.log(data.main.temp);
+        setTemp(data.main.temp);
+
     } catch (err) {
         console.error(err);
     }}
@@ -25,8 +29,8 @@ useEffect(() => {
     return ( 
     <div>
         <h1>Indianapolis</h1>
-        <p>Temperature: ºF</p>
-        <p>Temperature: ºC</p>
+        <p>Temperature: {fahrenheit}ºF</p>
+        <p>Temperature: {celsius}ºC</p>
             <button>Change Unit</button>
     </div> 
     );
