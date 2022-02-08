c2ac3132192e9dc14d442939b0d44fae981bf177
Maureen McCarty <stardustgirl@gmail.com>
2022-02-08 10:29:41 -0500

    fixed image size

--- src/Nasa.jsx
+++ src/Nasa.jsx
@@ -20,8 +20,8 @@ const Nasa = (props) => {
 
      return ( <div>
          <h1>Hello from Nasa</h1>
-         <img src={picture} />
-         <button onClick={handleFetch}>Click for new picture</button>
+         <img src={picture} width={300} height={300}  />
+         {/* <button onClick={handleFetch}>Click for picture</button> */}
      </div> );
  }
   
