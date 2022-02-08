be3e3de490c672f54f9c45f9bede1c8b047766dd
TaniaOlivas <taniaolivas24@gmail.com>
2022-02-08 09:37:46 -0500

    Fetch was added, it retrieving data via set coordinates, and a full object is displaying in the console

--- src/TicketMaster.jsx
+++ src/TicketMaster.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const baseURL =
+  'https://app.ticketmaster.com/discovery/v2/events.json?latlong=34.0522,-118.2437';
+const key = 'NBPgGHYWCXQ2SmBwuPu60TIwceuXMODW';
 
 const TicketMaster = (props) => {
+  const [name, setName] = useState('');
+
+  async function handleFetch() {
+    let url = `${baseURL}&apikey=${key}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setName(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <div>
       <h1>Hello from TicketMaster</h1>
+      <button onClick={handleFetch}>Click here for Event!</button>
     </div>
   );
 };
