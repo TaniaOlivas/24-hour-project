857c7e37ecefd0513c0cc5571f9f5b5b3a1843a1
TaniaOlivas <taniaolivas24@gmail.com>
2022-02-08 10:06:30 -0500

    Added the location function to track ask location upon getting to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TicketMaster from './TicketMaster';
 
 function App() {
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
+  const [status, setStatus] = useState('null');
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus('Unable to retrieve your location');
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <div>
       <TicketMaster />
+      <h1>Coordinates</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
     </div>
   );
 }
